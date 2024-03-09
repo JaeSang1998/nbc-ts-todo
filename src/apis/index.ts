103a247bc40aa5fb49914837ff6fe92b6368f832
jaesang1998 <tunaonchess01@gmail.com>
2024-03-09 10:25:55 +0900

    lv3

---
+++ src/apis/index.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { AddTodo, Todo, ToggleTodo } from "../store/todoReducers";
+
+const URL = "http://localhost:4000/todos";
+
+export const fetchTodo = async () => {
+  const { data } = await axios.get<Todo[]>(URL);
+  return data;
+};
+
+export const addTodo = async (todo: AddTodo) => {
+  const { data } = await axios.post<Todo>(URL, todo);
+  return data;
+};
+
+export const toggleTodo = async ({ id, isDone }: ToggleTodo) => {
+  const { data } = await axios.patch<Todo>(`${URL}/${id}`, { isDone: !isDone });
+  return data;
+};
+
+export const deleteTodo = async (id: Todo["id"]) => {
+  await axios.delete(`${URL}/${id}`);
+};
