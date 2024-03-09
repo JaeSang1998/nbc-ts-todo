5c860abf4f5b8f1aabe9f1fbc9cecca18cd92193
jaesang1998 <tunaonchess01@gmail.com>
2024-03-09 09:08:06 +0900

    lv4

--- src/store/todoReducers.ts
+++ src/store/todoReducers.ts
@@ -1,5 +1,38 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
+const URL = "http://localhost:4000/todos";
+export const fetchTodos = createAsyncThunk("todos/fetchTodos", async () => {
+  const response = await axios.get(URL);
+  return response.data;
+});
+
+export const addTodo = createAsyncThunk(
+  "todos/addTodo",
+  async (title: string) => {
+    const response = await axios.post(URL, { title, isDone: false });
+    return response.data;
+  }
+);
+
+export const toggleTodo = createAsyncThunk(
+  "todos/toggleTodo",
+  async (id: string) => {
+    const todo = await axios.get(`${URL}/${id}`);
+    const response = await axios.patch(`${URL}/${id}`, {
+      isDone: !todo.data.isDone,
+    });
+    return response.data;
+  }
+);
+
+export const deleteTodo = createAsyncThunk(
+  "todos/deleteTodo",
+  async (id: string) => {
+    await axios.delete(`${URL}/${id}`);
+    return id;
+  }
+);
 export type Todo = {
   id: string;
   title: string;
@@ -16,24 +49,27 @@ const initialState: TodoState = { value: [] };
 export const todosSlice = createSlice({
   name: "todo",
   initialState,
-  reducers: {
-    setTodo: (state, { payload }: PayloadAction<Todo[]>) => {
-      state.value = payload;
-    },
-    addTodo: (state, { payload }: PayloadAction<Todo>) => {
-      state.value.push(payload);
-    },
-    toggleTodo: (state, { payload }: PayloadAction<Todo["id"]>) => {
-      const index = state.value.findIndex((todo) => todo.id === payload);
-      if (index !== -1) {
-        state.value[index].isDone = !state.value[index].isDone;
-      }
-    },
-    deleteTodo: (state, { payload }: PayloadAction<Todo["id"]>) => {
-      state.value = state.value.filter((todo) => todo.id !== payload);
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTodos.fulfilled, (state, action) => {
+        state.value = action.payload;
+      })
+      .addCase(addTodo.fulfilled, (state, action) => {
+        state.value.push(action.payload);
+      })
+      .addCase(toggleTodo.fulfilled, (state, action) => {
+        const index = state.value.findIndex(
+          (todo) => todo.id === action.payload.id
+        );
+        if (index !== -1) {
+          state.value[index] = action.payload;
+        }
+      })
+      .addCase(deleteTodo.fulfilled, (state, action) => {
+        state.value = state.value.filter((todo) => todo.id !== action.payload);
+      });
   },
 });
 
-export const { setTodo, addTodo, toggleTodo, deleteTodo } = todosSlice.actions;
 export default todosSlice.reducer;
